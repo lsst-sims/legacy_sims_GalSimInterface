32d93a0c2b7923a45499718e6261c333977a8c8f
danielsf <scott.f.daniel@gmail.com>
2015-07-21 14:40:34 -0700

    adding more data to catalog for tests

--- python/lsst/sims/GalSimInterface/galSimCatalogs.py
+++ python/lsst/sims/GalSimInterface/galSimCatalogs.py
@@ -113,13 +113,15 @@ class GalSimBase(InstanceCatalog, CameraCoords, PhotometryHardware):
     #do not land on any detectors
     cannot_be_null = ['sedFilepath', 'fitsFiles']
 
-    column_outputs = ['galSimType', 'uniqueId', 'chipName', 'x_pupil', 'y_pupil', 'sedFilepath',
+    column_outputs = ['galSimType', 'uniqueId', 'chipName', 'xPix', 'yPix', 'raObserved', 'decObserved', 'x_pupil', 'y_pupil', 'sedFilepath',
                       'majorAxis', 'minorAxis', 'sindex', 'halfLightRadius',
                       'positionAngle','fitsFiles']
 
     transformations = {'x_pupil':arcsecFromRadians,
                        'y_pupil':arcsecFromRadians,
-                       'halfLightRadius':arcsecFromRadians}
+                       'halfLightRadius':arcsecFromRadians,
+                       'raObserved':numpy.degrees,
+                       'decObserved':numpy.degrees}
 
     default_formats = {'S':'%s', 'f':'%.9g', 'i':'%i'}
 
