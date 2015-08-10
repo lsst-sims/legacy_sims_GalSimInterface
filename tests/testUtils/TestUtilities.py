ac88afff93c3fcc94cbdcdd8776234da6b3f1fd2
danielsf <scott.f.daniel@gmail.com>
2015-08-10 15:57:39 -0700

    document create_text_catalog

--- tests/testUtils/TestUtilities.py
+++ tests/testUtils/TestUtilities.py
@@ -7,24 +7,45 @@ from lsst.sims.utils import radiansFromArcsec
 __all__ = ["create_text_catalog"]
 
 
-def create_text_catalog(obs, file_name, xDisplacement, yDisplacement, hlr=None, mag_norm=None):
+def create_text_catalog(obs, file_name, raDisplacement, decDisplacement, hlr=None, mag_norm=None):
+    """
+    Create a text file containing objects that can be read in by a fileDBObject class.
+
+    @param [in] obs is an ObservationMetaData specifying the pointing on which the catalog
+    will be centered
+
+    @param [in] file_name is the name of the file to be created.  If a file already exists with that
+    name, throw an error.
+
+    @param [in] raDisplacement is a numpy array listing the RA displacements of objects from the
+    pointing's center in arcseconds
+
+    @param [in] decDisplacement is a numpy array listing the Dec displacements of objects from the
+    pointings' center in arcseconds
+
+    @param [in] hlr is an optional list of half light radii in arcseconds
+
+    @param [in] mag_norm is an optional list of the objects' magnitude normalizations
+    """
 
     if os.path.exists(file_name):
-        os.unlink(file_name)
+        raise RuntimeError("file %s already exists; create_text_catalog will not overwrite it" % file_name)
 
-    dxList = radiansFromArcsec(xDisplacement)
-    dyList = radiansFromArcsec(yDisplacement)
+    raDisplacementList = radiansFromArcsec(raDisplacement)
+    decDisplacementList = radiansFromArcsec(decDisplacement)
 
     if hlr is None:
-        hlr = [2.0]*len(dxList)
+        hlr = [2.0]*len(raDisplacement)
 
     if mag_norm is None:
-        mag_norm = [21.0]*len(dxList)
+        mag_norm = [21.0]*len(raDisplacement)
 
 
     with open(file_name,'w') as outFile:
         outFile.write('# test_id ra dec hlr mag_norm\n')
-        for ix, (dx, dy, halfLight, magNorm) in enumerate(zip(dxList, dyList, hlr, mag_norm)):
+
+        for ix, (dx, dy, halfLight, magNorm) in \
+        enumerate(zip(raDisplacementList, decDisplacementList, hlr, mag_norm)):
 
             rr = numpy.degrees(obs._unrefractedRA+dx)
             dd = numpy.degrees(obs._unrefractedDec+dy)
