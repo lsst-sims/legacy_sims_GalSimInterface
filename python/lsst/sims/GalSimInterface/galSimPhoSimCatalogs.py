bd910f02073a5643a22114ed428e2d3b003aa47d
danielsf <scott.f.daniel@gmail.com>
2016-01-11 10:45:00 -0800

    added classes that generate images with GalSim, but output catalogs formatted for PhoSim.
    This functionality is currently untested by unit tests

---
+++ python/lsst/sims/GalSimInterface/galSimPhoSimCatalogs.py
@@ -0,0 +1,74 @@
+from lsst.sims.catUtils.exampleCatalogDefinitions import PhoSimCatalogPoint
+from lsst.sims.catUtils.exampleCatalogDefinitions import PhoSimCatalogZPoint
+from lsst.sims.catUtils.exampleCatalogDefinitions import PhoSimCatalogSersic2D
+
+from lsst.sims.GalSimInterface import GalSimStars, GalSimGalaxies, GalSimAgn
+
+from lsst.sims.catalogs.measures.instance import compound
+from lsst.sims.catUtils.mixins import AstrometryGalaxies, AstrometryStars
+
+
+__all__ = ["GalSimPhoSimStars", "GalSimPhoSimGalaxies", "GalSimPhoSimAgn"]
+
+
+class GalSimAstrometryStars(AstrometryStars):
+
+    @compound('raPhoSim','decPhoSim')
+    def get_phoSimCoordinates(self):
+        ff = self.column_by_name('fitsFiles')
+        return self.observedStellarCoordinates(includeRefraction = False)
+
+
+class GalSimAstrometryGalaxies(AstrometryGalaxies):
+
+    @compound('raPhoSim','decPhoSim')
+    def get_phoSimCoordinates(self):
+        ff = self.column_by_name('fitsFiles')
+        ra = self.column_by_name('raJ2000')
+        dec = self.column_by_name('decJ2000')
+        return _observedFromICRS(ra, dec, includeRefraction = False, obs_metadata=self.obs_metadata,
+                                epoch=self.db_obj.epoch)
+
+
+class GalSimPhoSimStars(GalSimAstrometryStars, PhoSimCatalogPoint, GalSimStars):
+
+    default_columns = [('redshift', 0., float),('shear1', 0., float), ('shear2', 0., float),
+                       ('kappa', 0., float), ('raOffset', 0., float), ('decOffset', 0., float),
+                       ('galacticExtinctionModel', 'CCM', (str,3)),
+                       ('internalExtinctionModel', 'none', (str,4)),
+                       ('galacticAv', 0.1, float),
+                       ('galSimType', 'pointSource', (str,11)),
+                       ('internalAv', 0.0, float),
+                       ('internalRv', 0.0, float),
+                       ('majorAxis', 0.0, float),
+                       ('minorAxis', 0.0, float),
+                       ('sindex', 0.0, float),
+                       ('positionAngle', 0.0, float),
+                       ('halfLightRadius', 0.0, float)]
+
+
+class GalSimPhoSimGalaxies(GalSimAstrometryGalaxies, PhoSimCatalogSersic2D, GalSimGalaxies):
+
+    default_columns = [('shear1', 0., float), ('shear2', 0., float), ('kappa', 0., float),
+                       ('raOffset', 0., float), ('decOffset', 0., float), ('galacticAv', 0.1, float),
+                       ('galacticExtinctionModel', 'CCM', (str,3)),
+                       ('internalExtinctionModel', 'CCM', (str,3)), ('internalAv', 0., float),
+                       ('internalRv', 3.1, float),
+                       ('galSimType', 'sersic', (str, 6))]
+
+
+class GalSimPhoSimAgn(GalSimAstrometryGalaxies, PhoSimCatalogZPoint, GalSimGalaxies):
+
+    default_columns = [('shear1', 0., float), ('shear2', 0., float), ('kappa', 0., float),
+                       ('raOffset', 0., float), ('decOffset', 0., float), ('spatialmodel', 'ZPOINT', (str, 6)),
+                       ('galacticExtinctionModel', 'CCM', (str,3)),
+                       ('galacticAv', 0.1, float),
+                       ('internalExtinctionModel', 'none', (str,4)),
+                       ('galSimType', 'pointSource', (str,11)),
+                       ('majorAxis', 0.0, float),
+                       ('minorAxis', 0.0, float),
+                       ('sindex', 0.0, float),
+                       ('positionAngle', 0.0, float),
+                       ('halfLightRadius', 0.0, float),
+                       ('internalAv', 0.0, float),
+                       ('internalRv', 0.0, float)]
