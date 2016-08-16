6e68726cfcfb2b9853f8183c924b0351bf9a1334
danielsf <scott.f.daniel@gmail.com>
2016-08-16 16:00:27 -0700

    minimal pytest support in testFWHM.py

--- tests/testFWHM.py
+++ tests/testFWHM.py
@@ -1,7 +1,7 @@
 import numpy
 import os
 import unittest
-import lsst.utils.tests as utilsTests
+import lsst.utils.tests
 from lsst.utils import getPackageDir
 import lsst.afw.image as afwImage
 from lsst.sims.utils import ObservationMetaData, radiansFromArcsec, arcsecFromRadians
@@ -13,6 +13,11 @@ from lsst.sims.coordUtils import _raDecFromPixelCoords
 from lsst.sims.coordUtils.utils import ReturnCamera
 
 from  testUtils import create_text_catalog
+
+
+def setup_module(module):
+    lsst.utils.tests.init()
+
 
 class fwhmFileDBObj(fileDBObject):
     idColKey = 'test_id'
@@ -197,16 +202,9 @@ class GalSimFwhmTest(unittest.TestCase):
             os.unlink(dbFileName)
 
 
+class MemoryTestClass(lsst.utils.tests.MemoryTestCase):
+    pass
 
-
-def suite():
-    utilsTests.init()
-    suites = []
-    suites += unittest.makeSuite(GalSimFwhmTest)
-
-    return unittest.TestSuite(suites)
-
-def run(shouldExit = False):
-    utilsTests.run(suite(), shouldExit)
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
