76c4b3223ef283c1f3781db1d83878a2e72ec137
danielsf <scott.f.daniel@gmail.com>
2016-12-19 13:52:56 -0800

    run unit test on more diverse pa, rotSky combinations

--- tests/testPositionAngle.py
+++ tests/testPositionAngle.py
@@ -173,53 +173,52 @@ class GalSimPositionAngleTest(unittest.TestCase):
         detName = detector.getName()
 
         rng = np.random.RandomState(42)
-        paList = rng.random_sample(2)*360.0
-        rotSkyPosList = rng.random_sample(2)*360.0
+        paList = rng.random_sample(7)*360.0
+        rotSkyPosList = rng.random_sample(77)*360.0
 
-        for pa in paList:
-            for rotSkyPos in rotSkyPosList:
+        for pa, rotSkyPos in zip(paList, rotSkyPosList):
 
-                imageName = '%s_%s_u.fits' % (imageRoot, detName)
+            imageName = '%s_%s_u.fits' % (imageRoot, detName)
 
-                obs = ObservationMetaData(pointingRA = 75.0,
-                                          pointingDec = -12.0,
-                                          boundType = 'circle',
-                                          boundLength = 4.0,
-                                          rotSkyPos = rotSkyPos,
-                                          mjd = 49250.0)
+            obs = ObservationMetaData(pointingRA = 75.0,
+                                      pointingDec = -12.0,
+                                      boundType = 'circle',
+                                      boundLength = 4.0,
+                                      rotSkyPos = rotSkyPos,
+                                      mjd = 49250.0)
 
-                create_text_catalog(obs, dbFileName,
-                                    rng.random_sample(1)*20.0-10.0,
-                                    rng.random_sample(1)*20.0-10.0,
-                                    pa=[pa],
-                                    mag_norm=[17.0])
+            create_text_catalog(obs, dbFileName,
+                                rng.random_sample(1)*20.0-10.0,
+                                rng.random_sample(1)*20.0-10.0,
+                                pa=[pa],
+                                mag_norm=[17.0])
 
-                db = paFileDBObj(dbFileName, runtable='test')
+            db = paFileDBObj(dbFileName, runtable='test')
 
-                cat = paCat(db, obs_metadata=obs)
-                cat.camera = camera
+            cat = paCat(db, obs_metadata=obs)
+            cat.camera = camera
 
-                cat.write_catalog(catName)
-                cat.write_images(nameRoot=imageRoot)
+            cat.write_catalog(catName)
+            cat.write_images(nameRoot=imageRoot)
 
-                paTest = self.get_position_angle(imageName, camera, detector, obs, 2000.0)
+            paTest = self.get_position_angle(imageName, camera, detector, obs, 2000.0)
 
-                # need to compare against all angles displaced by either 180 or 360 degrees
-                # from expected answer
-                deviation = np.abs(np.array([pa-paTest,
-                                             pa-180.0-paTest,
-                                             pa+180.0-paTest,
-                                             pa-360.0-paTest,
-                                             pa+360.0-paTest])).min()
+            # need to compare against all angles displaced by either 180 or 360 degrees
+            # from expected answer
+            deviation = np.abs(np.array([pa-paTest,
+                                         pa-180.0-paTest,
+                                         pa+180.0-paTest,
+                                         pa-360.0-paTest,
+                                         pa+360.0-paTest])).min()
 
-                self.assertLess(deviation, 2.0)
+            self.assertLess(deviation, 3.0)
 
-                if os.path.exists(catName):
-                    os.unlink(catName)
-                if os.path.exists(dbFileName):
-                    os.unlink(dbFileName)
-                if os.path.exists(imageName):
-                    os.unlink(imageName)
+            if os.path.exists(catName):
+                os.unlink(catName)
+            if os.path.exists(dbFileName):
+                os.unlink(dbFileName)
+            if os.path.exists(imageName):
+                os.unlink(imageName)
 
 
 class MemoryTestClass(lsst.utils.tests.MemoryTestCase):
