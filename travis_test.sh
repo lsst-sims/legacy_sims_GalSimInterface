193ad5ba51633a29961e3999a4ff7b686aab2202
Francois <fr.eiffel@gmail.com>
2018-03-19 19:30:05 -0400

    Added test data from sims_catUtils

--- travis_test.sh
+++ travis_test.sh
@@ -3,8 +3,9 @@ source scl_source enable devtoolset-6
 source loadLSST.bash
 setup lsst_sims
 # Get the test data for afw
-mkdir tmp; cd tmp; git clone https://github.com/lsst/afw.git ; cd .. 
+mkdir tmp; cd tmp; git clone https://github.com/lsst/afw.git ; https://github.com/lsst/sims_catUtils.git; cd ..
 ln -sf `pwd`/tmp/afw/tests /opt/lsst/stack/stack/miniconda3-4.3.21-10a4fa6/Linux64/afw/14.0-52-g19103d347+1/
+ln -sf `pwd`/tmp/sims_catUtils/tests /opt/lsst/stack/stack/miniconda3-4.3.21-10a4fa6/Linux64/sims_catUtils/2.6.0.sims/
 pip install nose
 pip install coveralls
 pip install pylint
