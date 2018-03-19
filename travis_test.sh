b5c0dcc0049b853cec60bc50aec48dc091631303
EiffL <fr.eiffel@gmail.com>
2018-03-19 16:10:29 -0700

    Added code necessary for travis test

--- travis_test.sh
+++ travis_test.sh
@@ -2,6 +2,9 @@
 source scl_source enable devtoolset-6
 source loadLSST.bash
 setup lsst_sims
+# Get the test data for afw
+mkdir tmp; cd tmp; git clone https://github.com/lsst/afw.git ; cd .. 
+ln -sf `pwd`/tmp/afw/tests /opt/lsst/stack/stack/miniconda3-4.3.21-10a4fa6/Linux64/afw/14.0-52-g19103d347+1/
 pip install nose
 pip install coveralls
 pip install pylint
